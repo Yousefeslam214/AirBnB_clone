cc5aa21fd0115128da1b8f4601a35ff67bf0712f
Yousef eslam <yousefeslam214@gmail.com>
2024-02-09 20:07:56 +0200

    done

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-import os
 import json
-
+import os
 
 class FileStorage():
     __file_path = "file.json"
-    __objects = {} #<class name>.id
+    __objects = {}
 
-    #@method.get()
     def all(self):
-        return FileStorage.__objects
+        """Returns the dictionary __objects"""
+        return self.__objects
 
-    #@set
     def new(self, obj):
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        print(FileStorage.__file_path)
-        with open(FileStorage.__file_path, 'w') as outfile:
-            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, outfile)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        objects_dict = {}
+        
+        for key, obj in self.__objects.items():
+            objects_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(objects_dict, file)
 
     def reload(self):
         """
@@ -27,11 +30,10 @@ class FileStorage():
         to __objects (only if the
         JSON file (__file_path)
         """
-        
+
         try:
             with open(self.__file_path, "r", encoding='utf-8') as f:
                 json_objs = json.load(f)
-
             for key, value in json_objs.items():
                 print(key)
                 print(value)
