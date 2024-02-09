f1bee9f379e659bd649fba0361a6983a0c15a7eb
Yousef eslam <yousefeslam214@gmail.com>
2024-02-09 20:23:31 +0200

    done

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 #when i test it return unarrangete values
 # i will investigate in it
 from models import storage
-
+import models
 
 class BaseModel():
     """
@@ -15,17 +15,20 @@ class BaseModel():
         """
         Public instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if len(kwargs) > 0:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
+                if key != "__class__":
+                    if key == "updated_at":
+                        self.updated_at = datetime.fromisoformat(value)
+                    elif key == "created_at":
+                        self.created_at = datetime.fromisoformat(value)
+                    else:
+                        setattr(self, key, value)
 
 
     def __str__(self):
@@ -48,4 +51,4 @@ class BaseModel():
     def save(self):
         """Public instance methods"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
