cc3213e4e7d40fd27461b6b6c1ed1bffaed5402f
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-02-12 12:37:54 -0800

    check

--- console.py
+++ console.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Defines the HBnB console."""
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
